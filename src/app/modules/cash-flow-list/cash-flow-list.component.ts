ee05bf73c0ec66bc383ea4509ddef2fd60bfc79a
elaiddabbebi <e.dabbebi@avidea.tn>
2024-02-09 21:33:50 +0100

    adjustments

--- src/app/modules/cash-flow-list/cash-flow-list.component.ts
+++ src/app/modules/cash-flow-list/cash-flow-list.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {TabViewChangeEvent} from "primeng/tabview";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-cash-flow-list',
   templateUrl: './cash-flow-list.component.html',
   styleUrls: ['./cash-flow-list.component.css']
 })
-export class CashFlowListComponent {
+export class CashFlowListComponent implements OnInit {
 
+  tabIndex: number = 0;
+  constructor(
+    private route: ActivatedRoute,
+    public location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      if (params['tab'] === 'cash-flow') {
+        this.tabIndex = 0;
+      } else if (params['tab'] === 'cash-flow-category') {
+        this.tabIndex = 1;
+      }
+    });
+  }
+
+  onTabChanged(tabEvent: TabViewChangeEvent): void {
+    let tabParam = '';
+    switch (tabEvent.index) {
+      case 0:
+        tabParam = 'cash-flow';
+        break;
+      case 1:
+        tabParam = 'cash-flow-category';
+        break;
+    }
+    if (tabParam !== '') {
+      const url = new URL(location.href);
+      const params = new URLSearchParams(url.search);
+      params.set('tab', tabParam);
+      url.search = params.toString();
+      this.location.replaceState(url.pathname + url.search);
+    }
+  }
 }
